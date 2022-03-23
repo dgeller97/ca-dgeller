7a5a0b155340fef1e4f858fd9cb4ef2c40e22480
David Geller <lugia2.2@hotmail.com>
2022-03-22 23:03:35 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+//import logo from './logo.svg';
+import './App.scss';
+
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+
+      <header className="App-header">
+        <h1>
+          David Geller
+        </h1>
+
+        <div className='menu'>
+          <ul>
+            <li>
+              <a href='/'>Home</a>
+            </li>
+            <li>
+              <a href='/about'>About</a>
+            </li>
+          </ul>
+        </div>
+
+      </header>
+
+      <div clasName='body'>
+        <img src="https://cdn.geekwire.com/wp-content/uploads/2019/05/Clippy.jpg.jpg" className="App-logo" alt="logo" />
+          <p>
+            Under construction
+          </p>
+      </div>
+    </div>
+  );
+
+
+}
+
+export default App;
